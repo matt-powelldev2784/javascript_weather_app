dbe3cc2ef86a0135975e1585aae2dd0a28410542
matt-powelldev2784 <matt.powell.dev2784@gmail.com>
2021-11-24 17:55:46 +0000

    unused current search variable removed

--- main.js
+++ main.js
@@ -2,10 +2,6 @@ import getGeoLocation from './getGeoLocation.js';
 import getWeather from './getWeather.js';
 import renderWeather from './renderWeather.js';
 import processWeatherData from './processWeatherData.js';
-import renderError from './renderError.js';
-
-// object to store current search values
-export const currentSearch = {};
 
 const getAddress = async () => {
   const searchInput = document.querySelector('.search__input');
